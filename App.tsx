496ee9a71c0a89572ec7ab9213e770a801d45cd9
Guilherme Pereira <guilhermepe90@gmail.com>
2025-07-28 17:07:42 -0300

    primeiro commit test

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { Text, View} from "react-native";
+import React, {Component} from "react";
+
+class App extends Component {
+
+    render(){
+        return (
+            <View style={{ marginTop: 20}}>
+                <Text>Texto 1</Text>
+                <Text>Texto 2</Text>
+                <Text>Texto 3</Text>
+                <Text>Texto 4</Text>
+            </View>
+        )
+    }
+
+}
+export default App;
+
---
+++ Lib/Style/styleSheets.tsx
@@ -0,0 +1,13 @@
+import app from "../../App.tsx";
+import { StyleSheet} from "react-native";
+
+
+class Estilos(){
+    const estilos = StyleSheets.create({
+        area: {
+            marginTop: 40,
+        }
+    });
+
+
+}
