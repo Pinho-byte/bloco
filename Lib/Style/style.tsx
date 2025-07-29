02dbdd5c5055b77658a206a610117df65196fac3
Guilherme Pereira <guilhermepe90@gmail.com>
2025-07-29 16:43:34 -0300

    adicionado mais arquivo para armazenar a parte de estilos

---
+++ Lib/Style/style.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet} from "react-native";
+
+
+const estilos = StyleSheet.create({
+    area: {
+        marginTop: 40,
+        marginBottom: 40,
+        backgroundColor: 'white',
+        flex: 1,
+    }
+})
+
+export default estilos;
