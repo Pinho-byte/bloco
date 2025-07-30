5657e7b680fccf0d42b1b63b478482a65f0d537f
Guilherme Pereira <guilhermepe90@gmail.com>
2025-07-30 16:48:12 -0300

    Foi adicionado o Style Botão e Text
    Signed-off-by: Guilherme Pereira <guilhermepe90@gmail.com>

--- Lib/Style/style.tsx
+++ Lib/Style/style.tsx
@@ -5,12 +5,21 @@ const estilos = StyleSheet.create({
     area: {
         marginTop: 40,
         marginBottom: 40,
-        backgroundColor: 'white',
+        backgroundColor: '',
         flex: 1,
     },
     botao: {
-
-
+        height: 45,
+        borderWidth: 1,
+        borderColor: 'black',
+        margin: 10,
+        fontSize: 20,
+        padding: 10,
+        textDecorationColor: 'black',
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 25,
     }
 })
 
