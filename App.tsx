cfb775f266f0f0754a021b914bc85e3c81d539e6
Guilherme Pereira <guilhermepe90@gmail.com>
2025-07-30 16:47:40 -0300

    Foi adicionado o Props com AppState usando o formato TypeScript
    Signed-off-by: Guilherme Pereira <guilhermepe90@gmail.com>

--- App.tsx
+++ App.tsx
@@ -1,19 +1,35 @@
-import {TextInput, View} from "react-native";
+import {Text, TextInput, View} from "react-native";
 import React, {Component} from "react";
 import estilos from "./Lib/Style/style.tsx";
+import {AppState} from "./Lib/components/types.ts";
 
-class App extends Component {
+//Interface de estado
+
+
+class App extends Component<{}, AppState> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            nome: ''
+        };
+        this.pegaNome = this.pegaNome.bind(this);
+    }
+    pegaNome(texto: string) {
+        this.setState({nome: texto});
+    }
 
     render() {
         return (
             <View style={estilos.area}>
-                <TextInput
-
+                <TextInput style={estilos.botao}
+                placeholder="Digite seu nome"
+                underlineColorAndroid="transparent"
+                onChangeText={this.pegaNome}
                 />
-                <TextInput
 
-                />
-
+                <Text style={estilos.text}>
+                    Bem Vindo {this.state.nome}</Text>
 
 
 
